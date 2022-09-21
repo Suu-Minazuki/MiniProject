4ee17fce9bd5357c99cd66d13b24d00e0d9f835d
Suu-Minazuki <02200147.cst@rub.edu.bt>
2022-09-21 21:31:24 +0600

    first changes from minazuki

--- app/src/main/java/com/example/miniproject/SignUpForm.java
+++ app/src/main/java/com/example/miniproject/SignUpForm.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class SignUpForm extends AppCompatActivity {
+
+    private ViewPager2 view_pager_2;
+    private PageAdapterForRegistration pageAdapterForRegistration;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signupform);
+
+        view_pager_2 = findViewById(R.id.view_pager_2);
+        pageAdapterForRegistration = new PageAdapterForRegistration(this);
+        view_pager_2.setAdapter(pageAdapterForRegistration);
+
     }
 }
