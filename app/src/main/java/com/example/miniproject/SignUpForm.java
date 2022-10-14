a80daa6839f12ca5ad6f98fc27394e1c0c69b680
ugyenshimid <02200171.cst@rub.edu.bt>
2022-10-14 10:05:15 -0700

    14/10

--- app/src/main/java/com/example/miniproject/SignUpForm.java
+++ app/src/main/java/com/example/miniproject/SignUpForm.java
@@ -7,24 +7,15 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-
-import com.example.miniproject.adapter.PageAdapterForRegistration;
 
 public class SignUpForm extends AppCompatActivity {
 
-    private ViewPager2 view_pager_2;
-    private PageAdapterForRegistration pageAdapterForRegistration;
     private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signupform);
-
-        view_pager_2 = findViewById(R.id.view_pager_2);
-        pageAdapterForRegistration = new PageAdapterForRegistration(this);
-        view_pager_2.setAdapter(pageAdapterForRegistration);
+        setContentView(R.layout.registration);
 
         textView = findViewById(R.id.textView_sign_in);
         textView.setOnClickListener(new View.OnClickListener() {
