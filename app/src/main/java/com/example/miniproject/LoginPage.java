e5641e658da0bec30e5f3ad669f07c6d12cf6bc3
Suu-Minazuki <02200147.cst@rub.edu.bt>
2022-10-14 17:36:41 +0600

    second changes from minazuki

--- app/src/main/java/com/example/miniproject/LoginPage.java
+++ app/src/main/java/com/example/miniproject/LoginPage.java
@@ -1,13 +1,23 @@
 package com.example.miniproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,24 +25,29 @@ import com.google.firebase.database.FirebaseDatabase;
 public class LoginPage extends AppCompatActivity {
 
     private TextView textView;
-    private EditText email_eT, password_eT;
-
-    private FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private DatabaseReference root = db.getReference();
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        final EditText email = findViewById(R.id.email_eT);
-        final EditText password = findViewById(R.id.password_eT);
-
-        email_eT = findViewById(R.id.email_eT);
-        password_eT = findViewById(R.id.password_eT);
-
-
         textView = findViewById(R.id.textView8);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,9 +55,33 @@ public class LoginPage extends AppCompatActivity {
             }
         });
     }
-    public void loginBtn(View view) {
-        startActivity(new Intent(getApplicationContext(), Events.class));
-        //String name = email_eT.getText().toString();
-        //root.setValue(name);
+
+    private void loginUserAccount() {
+        // validations for input email and password
+        if (TextUtils.isEmpty(loginEmail.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(loginPassword.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // signin existing user
+        firebaseAuth.signInWithEmailAndPassword(loginEmail.getText().toString(), loginPassword.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(getApplicationContext(), Events.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage() + "", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
+
 }
--- app/src/main/java/com/example/miniproject/SignUpForm.java
+++ app/src/main/java/com/example/miniproject/SignUpForm.java
@@ -1,16 +1,56 @@
 package com.example.miniproject;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.miniproject.Model.EventWithData;
+import com.example.miniproject.Model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.Random;
 
 public class SignUpForm extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView regisImage;
+    private EditText regisName, regisEmail, regisPassword, regisYear, regisDepartment, regisJob;
+    private Spinner spinner;
+    private Button regisButton;
+    private Uri selectedImageUri;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +58,35 @@ public class SignUpForm extends AppCompatActivity {
         setContentView(R.layout.registration);
 
         textView = findViewById(R.id.textView_sign_in);
+        regisImage = findViewById(R.id.regisImage);
+        regisName = findViewById(R.id.regisName);
+        regisEmail = findViewById(R.id.regisEmail);
+        regisPassword = findViewById(R.id.regisPassword);
+        regisYear = findViewById(R.id.regisYear);
+        regisDepartment = findViewById(R.id.regisCourse);
+        regisJob = findViewById(R.id.regisJob);
+        spinner = findViewById(R.id.spinner);
+        regisButton = findViewById(R.id.regisButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference("UserImage1"+new Random().nextInt(50));
+
+        regisImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+            }
+        });
+
+        regisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //registerNewUser();
+                uploadtofirebase();
+            }
+        });
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,4 +94,111 @@ public class SignUpForm extends AppCompatActivity {
             }
         });
     }
+
+    private void imageChooser()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            // do your operation from here....
+            if (data != null && data.getData() != null) {
+                selectedImageUri = data.getData();
+                Bitmap selectedImageBitmap = null;
+                try {
+                    selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                regisImage.setImageBitmap(selectedImageBitmap);
+            }
+        }
+    });
+
+    private void registerNewUser() {
+        // Validations for input email and password
+        if (TextUtils.isEmpty(regisEmail.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(regisPassword.getText().toString())) {
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        // create new user or register new user
+        firebaseAuth.createUserWithEmailAndPassword(regisEmail.getText().toString(), regisPassword.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //uploadtofirebase();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage() + "", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void uploadtofirebase() {
+
+        if (selectedImageUri != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // adding listeners on upload
+            // or failure of image
+            storageReference.putFile(selectedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            // Image uploaded successfully
+                            // Dismiss dialog
+                            progressDialog.dismiss();
+                            FirebaseDatabase db=FirebaseDatabase.getInstance();
+                            DatabaseReference root=db.getReference("User");
+                            UserModel userModel  = new UserModel(
+                                    uri.toString(),
+                                    regisName.getText().toString(),
+                                    regisEmail.getText().toString(),
+                                    regisYear.getText().toString(),
+                                    regisDepartment.getText().toString(),
+                                    regisJob.getText().toString(),
+                                    "Student");
+                            root.child("Student").child("10601004816").setValue(userModel);
+                            Toast.makeText(SignUpForm.this, "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), LoginPage.class));
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Error, Image not uploaded
+                    progressDialog.dismiss();
+                    Toast.makeText(SignUpForm.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                // Progress Listener for loading
+                // percentage on the dialog box
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded " + (int)progress + "%");
+                }
+            });
+        }
+    }
+
 }
