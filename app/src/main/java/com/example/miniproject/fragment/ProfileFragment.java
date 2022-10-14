ef9c21d93173cb59e86fd7b6a92ce2aa31b19831
Suu-Minazuki <02200147.cst@rub.edu.bt>
2022-10-14 23:51:26 +0600

    second changes from minazuki

--- app/src/main/java/com/example/miniproject/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/miniproject/fragment/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,14 +18,17 @@ import android.widget.Toast;
 import com.example.miniproject.Model.EventWithData;
 import com.example.miniproject.Model.UserModel;
 import com.example.miniproject.R;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,11 +36,9 @@ public class ProfileFragment extends Fragment {
 
     private CircleImageView profileImage;
     private TextView profileName, profileDepartment, profileYear, profileDescription, profileEmail;
-    private ImageButton editP;
+    private ImageButton editP, logBtn;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
-    private String test;
-
     public static final String SHARED_PREFS = "sharedPrefs";
 
     @Override
@@ -52,16 +54,30 @@ public class ProfileFragment extends Fragment {
         profileDescription = view.findViewById(R.id.profileDescription);
         profileEmail = view.findViewById(R.id.profileEmail);
         editP = view.findViewById(R.id.editBtn);
+        logBtn = view.findViewById(R.id.logBtn);
 
         SharedPreferences sharedPreferences = getContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         String eMail = sharedPreferences.getString("email", "");
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("User");
-        Query query = databaseReference.orderByChild("userEmail").equalTo(eMail);
+        Query query = databaseReference.child("Student").orderByChild("userEmail").equalTo(eMail);
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                    assert userModel != null;
+                    Picasso.get().load(userModel.getUserImage()).placeholder(R.drawable.ic_baseline_image_24).into(profileImage);
+                    profileName.setText(userModel.getUserName());
+                    profileDepartment.setText(userModel.getUserDepartment());
+                    profileYear.setText(userModel.getUserYear());
+                    if (userModel.getDescription() == "Description"){
+                        profileDescription.setText("No Description");
+                    }else {
+                        profileDescription.setText(userModel.getDescription());
+                    }
+                    profileEmail.setText(userModel.getUserEmail());
+                }
             }
 
             @Override
@@ -70,10 +86,17 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
         editP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), query.toString() + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
             }
         });
 
